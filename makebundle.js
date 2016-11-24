0d2b20d4e7a8b75dc9794a31fb732d6be5fe3f25
huangzhengjie <huangzhengjie@kuaiqiangche.com>
2016-11-24 13:19:25 +0800

    打包bundle

--- makebundle.js
+++ makebundle.js
@@ -1,23 +1,19 @@
+var sys = require('sys');
+var count = 0;  
+sys.debug("开始进行打包……");  
+
+function timer_tick() {  
+     
+    sys.debug("正在打包");  
+    if (count === 2) {  
+        sys.debug("已打包完成");  
+    }  else {
+    	setTimeout(timer_tick, 1000);
+    }
+}  
 
 
-// var spawn = require('child_process').spawn, 
-// free = spawn('free', ['-m']); 
-
-// // 捕获标准输出并将其打印到控制台 
-// free.stdout.on('data', function (data) { 
-// console.log('标准输出：\n' + data); 
-// }); 
-
-// // 捕获标准错误输出并将其打印到控制台 
-// free.stderr.on('data', function (data) { 
-// console.log('标准错误输出：\n' + data); 
-// }); 
-
-// // 注册子进程关闭事件 
-// free.on('exit', function (code, signal) { 
-// console.log('子进程已退出，代码：' + code); 
-// }); 
-
+timer_tick();
 
 var exec = require('child_process').exec, 
 android_proc = exec('react-native bundle --entry-file index.android.js --bundle-output ./bundle/android/index.android.bundle --platform android --assets-dest ./bundle/android --dev false'); 
@@ -36,6 +32,7 @@ console.log('标准错误输出：\n' + data);
 
 // 注册子进程关闭事件 
 android_proc.on('exit', function (code, signal) { 
+	count++; 
 console.log('子进程已退出，代码：' + code); 
 }); 
 
@@ -52,5 +49,6 @@ console.log('标准错误输出：\n' + data);
 
 // 注册子进程关闭事件 
 ios_proc.on('exit', function (code, signal) { 
+	count++; 
 console.log('子进程已退出，代码：' + code); 
 }); 
